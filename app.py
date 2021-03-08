a25611bee83bab9b6e98680f21b8402e3b944471
adityakumarsahu0707 <adityakumarsahu45@gmail.com>
2021-03-08 12:15:25 +0530

    test page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask('test')
 
 @app.route('/')
 def index():
     return "Flask Model"
     #return("Index page!!")
+@app.route("/test")
+def test():
+    return render_template("test.html")
